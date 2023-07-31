3a2ce259abed31c647c8c6a133b58610c0703f49
Stanislav Kletsov <klecov@gmail.com>
2023-07-31 10:00:30 +0500

    Step 2

--- gendiff.js
+++ gendiff.js
@@ -7,6 +7,7 @@ const program = new Command();
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .version('0.1');
+  .version('0.1')
+  .option('-f, --format <type>', 'output format')
 
 program.parse();
